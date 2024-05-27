e8df56edf434a97be0d3ffe12921186fda97f2cc
Jimmys <90797256+jimmysrafael@users.noreply.github.com>
2024-05-26 21:26:11 -0400

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -155,7 +155,7 @@ function App() {
 
   useEffect(() => {
     async function getValores() {
-      const response = await axios.get("https://projeto-lumi-back-ajrx8b3q2-devjjs-projects-dd81c9bd.vercel.app/lumi");
+      const response = await axios.get("https://projeto-lumi-back-ajrx8b3q2-devjjs-projects-dd81c9bd.vercel.app:4000/lumi");
       const valores = response.data;
 
       // Criar um conjunto (Set) para armazenar valores únicos de numerocliente
